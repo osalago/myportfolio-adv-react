c2be415fe4db6819dd90d93903bfc8434248da55
osalago <oliversala@bluewn.ch>
2025-12-14 20:19:28 +0100

    feat: adapted footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Pete • © 2022</p>
+          <p>Oliver • © 2025</p>
         </Flex>
       </footer>
     </Box>
